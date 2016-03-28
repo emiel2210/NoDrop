c5747c09f72b38c65c002fde037acdfaeaf5341e
Jake <jakehcasto@aol.com>
2016-03-28 09:44:31 -0400

    Update Main.php

--- src/ImagicalGamer/NoDrop/Main.php
+++ src/ImagicalGamer/NoDrop/Main.php
@@ -8,8 +8,19 @@ class Main extends PluginBase implements Listener{
 
 public function onEnable(){
 	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder());
+	$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+	$config->set("NoDrop" . "true");
+	$config->save();
 }
 public function onDropItem(PlayerDropItemEvent $event){
+	$config = $this->getConfig();
+	if($config->get("NoDrop") === "true")
+	{
     $event->setCancelled(true);
 }
+   else
+   {
+   	$event->setCancelled(false);
+   }
 }
