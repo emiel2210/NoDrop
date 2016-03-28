472b4c2bb8974ff5b126d8d3b54f1467d174aae6
Jake <jakehcasto@aol.com>
2016-03-28 09:49:30 -0400

    Update Main.php

--- src/ImagicalGamer/NoDrop/Main.php
+++ src/ImagicalGamer/NoDrop/Main.php
@@ -4,25 +4,12 @@ use pocketmine\command\Command;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
 class Main extends PluginBase implements Listener{
 
 public function onEnable(){
 	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-        @mkdir($this->getDataFolder());
-	$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
-	$config->set("NoDrop" . "true");
-	$config->save();
 }
 public function onDropItem(PlayerDropItemEvent $event){
-	$config = $this->getConfig();
-	if($config->get("NoDrop") === "true")
-	{
     $event->setCancelled(true);
-}
-   else
-   {
-   	$event->setCancelled(false);
-   }
 }
 }
